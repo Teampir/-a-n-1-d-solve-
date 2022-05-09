3beb73a5a68701f34378cbd1d88d4e94ff0e1e67
Mrpirates <104631217+Mpirates@users.noreply.github.com>
2022-05-09 07:15:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -967,7 +967,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.main_TXT,
+            text=script.MAIN_TXT,
             disable_web_page_preview=True,
             reply_markup=reply_markup,
             parse_mode='html'
