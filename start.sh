c08dec6fe0553cef0141fee505aad4d137fe51e3
Distenyteam <105855119+Distenyteam@users.noreply.github.com>
2022-05-19 15:41:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Mpirates/allinone.git /Ajax
+  git clone https://github.com/Distenyteam/ql.git /Ajax
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Ajax
 fi
 cd /Ajax
 pip3 install -U -r requirements.txt
-echo "Starting ᗩᒍᗩ᙭....🔥"
+echo "Starting..🔥"
 python3 bot.py
