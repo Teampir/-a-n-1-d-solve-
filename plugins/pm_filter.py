064cb0775e71c973fdc316178e62f15d10ba4ca0
Distenyteam <105855119+Distenyteam@users.noreply.github.com>
2022-05-20 21:53:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1569,7 +1569,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ],
                 [
                     InlineKeyboardButton('Filter', callback_data=f'setgs#botpm#{settings["botpm"]}#{str(grp_id)}'),
-                    InlineKeyboardButton('PM' if settings["botpm"] else 'CHAT',
+                    InlineKeyboardButton('PM' else settings["botpm"] if 'CHAT',
                                          callback_data=f'setgs#botpm#{settings["botpm"]}#{str(grp_id)}')
                 ],
                 [
