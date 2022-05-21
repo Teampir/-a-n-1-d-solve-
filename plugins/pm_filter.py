fd11a1aaf44dc1188102c3b3ffce9906521436ca
Distenyteam <105855119+Distenyteam@users.noreply.github.com>
2022-05-21 12:06:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -495,7 +495,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
     
     elif query.data.startswith("Chat"):
-        ident, file_id, rid = query.data.split("#")
+        ident, file_id = query.data.split("#")
 
         if int(rid) not in [query.from_user.id, 0]:
             return await query.answer("⚠️ Bro, search your on file, don't click others request file🥵  ⚠️Bro മറ്റുള്ളവർ റിക്വസ്റ്റ് ചെയ്ത മൂവിയിൽ കുത്തി നോക്കാതെ ഡ്രോയിങ് വേണ്ടത് ബ്രോ റിക്വസ്റ്റ് ചെയ്യുക🤒.", show_alert=True)
