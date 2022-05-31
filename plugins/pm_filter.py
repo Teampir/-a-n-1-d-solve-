72b390946885e4b27e76944644045a2e2821ffff
Distenyteam <105855119+Distenyteam@users.noreply.github.com>
2022-05-31 23:03:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2120,7 +2120,6 @@ async def advantage_spell_chok(msg):
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
     m = await msg.reply_photo("I couldn't find anything related to that\nDid you mean any one of these?",
-            photo=random.choice(PICS),
                     reply_markup=InlineKeyboardMarkup(btn))
     await asyncio.sleep(20)
     await m.delete()
