dbf89fb087e6b8531dbeedc06c3fc3374e74a4f5
Distenyteam <105855119+Distenyteam@users.noreply.github.com>
2022-06-01 15:22:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2160,15 +2160,15 @@ async def advantage_spell_chok(msg):
                     callback_data='tips',
                 ),
             ]]
-    btn.append([InlineKeyboardButton(
+    btn.append([[InlineKeyboardButton(
             text=movie.strip(),
             callback_data=f"spolling#{user}#{k}",
         )
-    ] for k, movie in enumerate(movielist))
+    ] for k, movie in enumerate(movielist)])
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
     m = await msg.reply_photo(photo="https://telegra.ph/file/d72ee300dd67e81fc930e.jpg", caption=f"Hey, {msg.from_user.mention} I couldn't find anything related to that\nDid you mean any one of these?",
                     reply_markup=InlineKeyboardMarkup(btn))
-    await asyncio.sleep(20)
+    await asyncio.sleep(10)
     await m.delete()
 
 async def manual_filters(client, message, text=False):
