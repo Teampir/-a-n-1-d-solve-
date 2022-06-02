45aa108eec6c1c5aee559f9243a868339c320ea9
Distenyteam <105855119+Distenyteam@users.noreply.github.com>
2022-06-02 17:18:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2161,7 +2161,7 @@ async def advantage_spell_chok(msg):
             text=movie.strip(),
             callback_data=f"spolling#{user}#{k}",
         )
-    ] for k, movie in enumerate(movielist))
+    ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="🔐Close", callback_data=f'spolling#{user}#close_spellcheck'),
                 InlineKeyboardButton(text="ɪɴꜰᴏ", callback_data='tips')])
     m = await msg.reply_photo(photo="https://telegra.ph/file/d72ee300dd67e81fc930e.jpg", caption=f"Hey, {msg.from_user.mention} I couldn't find anything related to that\nDid you mean any one of these?",
